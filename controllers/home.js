ad2e7e82706e9c83626e6a0908e5adfe3a36dbcd
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-03 11:51:01 -0800

    Add add new twizzle functionity

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,16 @@
 Twizzler.HomeController = Ember.ArrayController.extend({
   sortProperties: ["datestamp"],
-  // actions: {
-  //   sortByDate: function() {
-  //     this.set("sortProperties", ["datestamp"]);
-  //   }
-  // }
+  actions: {
+    newTwizzle: function() {
+      twizzles.addObject({
+        id: (twizzles.length + 1),
+        twizzard: this.twizzard,
+        twizzle: this.twizzle,
+        datestamp: Date.now()
+      });
+    }
+    // sortByDate: function() {
+    //   this.set("sortProperties", ["datestamp"]);
+    // }
+  }
 });
